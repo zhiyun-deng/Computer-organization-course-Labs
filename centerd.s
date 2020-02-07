29719f26ae0ee6f6437f7fbeae6113e03d477970
l <luke.wenjun.ma@gmail.com>
2020-02-07 15:15:22 -0500

    updated files updated files

---
+++ centerd.s
@@ -0,0 +1,43 @@
+			.text 
+			.global _start 
+
+_start:
+			LDR R0, LENGTH 		//R0 stores the signal length (the number of element in list)
+			LDR R5, LENGTH		//stores signal length as used to calculated shift values
+			
+			LDR R2, =SIGNAL		//points to first number
+			LDR R6, =SIGNAL		//points to first number in memory for changing it after calculating average
+			MOV R3, #0			//sum
+			MOV R4, #0			//shift value
+
+//loop 1 sums all the numbers
+LOOP1:		SUBS R0, R0, #1 	//decrement the loop counter
+			BMI LOOP2 			//end loop if counter has reached zero
+			LDR R1, [R2]		//acessing element pointed by R2
+			ADD R3, R3, R1		//adding element to sum
+			ADD R2, R2, #4		//incrementing R2
+			B LOOP1 			//branch back to the loop
+
+//loop2 calcs shift val
+LOOP2:		LSRS R5, R5, #1		//triggers flag when r5 = 0
+			BEQ DONE1
+			ADD R4, R4, #1		//increment counter
+			B LOOP2
+
+DONE1:		LSR R3, R3, R4		//R3 now contains average
+			LDR R5, LENGTH		//reset to use as counter
+
+LOOP3:		SUBS R5, R5, #1
+			BMI END				//if neg bc subs is at top of loop & no done condition
+			LDR R0, [R6]		//temp reg to do ops
+			SUB R0, R0, R3
+			STR R0, [R6], #4	//forward
+			B LOOP3
+			
+
+END:		B END 				//infinite loop
+
+LENGTH:		.word 8 			//memory assigned for result location
+SIGNAL:		.word 1, 5, 3, 11, 676, 87, 74,63 	//the list data
+
+
