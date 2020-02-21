97cd29f67b83f07e19d89ad1f2368ef97223aa59
l <luke.wenjun.ma@gmail.com>
2020-02-21 16:45:17 -0500

    part 2 lab

--- lab2/fibonacci.s
+++ lab2/fibonacci.s
@@ -4,25 +4,31 @@
 _start:
 			MOV R0, #10
 			STR LR, [SP, #-4]!
+
 			BL FIBONACCI
 			LDR LR, [SP], #4
+
 END:		B END 				//infinite loop
 			//R0 is the parameter
+
 FIBONACCI: 	PUSH {R1-R3}
 			MOV R3, R0		//saves the parameter
 			STR LR, [SP, #-4]!
 			CMP R0, #2
 			BLT ONE
 			SUB R0, R0, #1
+
 			BL FIBONACCI
 			MOV R1, R0
 			SUB R0, R3, #2
+
 			BL FIBONACCI
 			MOV R2, R0
 			ADD R0, R1, R2
 			LDR LR, [SP], #4
 			POP {R1-R3}
 			BX LR
+
 ONE:		MOV R0, #1
 			LDR LR, [SP], #4
 			POP {R1-R3}
